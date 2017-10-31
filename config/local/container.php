6f0f63a0def57fee086e45db1f194842fe4ddf06
Gustavo Delgado <gustavod@digintent.com>
2017-10-30 22:40:15 -0300

    Remove local config

--- config/local/container.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use DI\ContainerBuilder;
-
-$containerBuilder = new ContainerBuilder;
-$containerBuilder->addDefinitions(__DIR__ . '/services.php');
-$container = $containerBuilder->build();
-
-return $container;
